a136ee2797cf591016ebce24700954829d6f65b1
Zhimin Peng <zhiminp@gmail.com>
2015-08-04 22:22:41 -0700

    code for convert Roman to integer

---
+++ elem_prog_interview/ch7/q7.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+
+int RomanToInt(const string& s) {
+
+  map<char, int> T = {{'I', 1},
+                      {'V', 5},
+                      {'X', 10},
+                      {'L', 50},
+                      {'C', 100},
+                      {'D', 500},
+                      {'M', 1000}};
+
+  int sum = T[s.back()];
+
+  for (int i = s.size() - 2; i >= 0 ; --i) {
+    if (T[s[i]] < T[s[i + 1]]) {
+      sum -= T[s[i]];
+    } else {
+      sum += T[s[i]];
+    }
+  }
+  return sum; 
+}
