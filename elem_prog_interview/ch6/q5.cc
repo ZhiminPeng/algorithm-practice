7b2c15cfb70dbc1a739c978ebd1324388e147f4a
Zhimin Peng <zhiminp@gmail.com>
2015-08-11 20:39:43 -0700

    small change

--- elem_prog_interview/ch6/q5.cc
+++ elem_prog_interview/ch6/q5.cc
@@ -4,6 +4,8 @@
 using namespace std;
 
 size_t removeElement(vector<int>& A, int k) {
+  // write-idx keep track of the next available slot for
+  // fill in value
   size_t write_idx = 0;
   for (size_t i = 0; i < A.size(); ++i) {
     if (A[i] != k) {
