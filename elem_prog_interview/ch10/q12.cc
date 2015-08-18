9ef7d6c6590d18d9939810a09f26375aa077ca12
Zhimin Peng <zhiminp@gmail.com>
2015-08-17 23:46:10 -0700

    added the binary construction with post order and pre order traversal

--- elem_prog_interview/ch10/q12.cc
+++ elem_prog_interview/ch10/q12.cc
@@ -33,3 +33,30 @@ public:
         
     }
 };
+
+// create a binary tree with post order and inorder traversal
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        
+        return buildTreeHelper(inorder, postorder, 0, inorder.size(), 0, postorder.size());
+        
+    }
+    TreeNode* buildTreeHelper(vector<int>& inorder, vector<int>& postorder, int in_start, int in_end, int post_start, int post_end) {
+        if (in_start >= in_end || post_start >= post_end) {
+            return NULL;
+        }
+        TreeNode* root = new TreeNode(postorder[post_end - 1]);
+        int i = 0;
+        for (i = in_start; i < in_end; i++) {
+            if (inorder[i] == postorder[post_end - 1]) break;
+        }
+        int len = i - in_start; 
+        TreeNode* left = buildTreeHelper(inorder, postorder, in_start, in_start + len, post_start, post_start + len);
+        TreeNode* right = buildTreeHelper(inorder, postorder, i + 1, in_end, post_start + len, post_end - 1);
+        root->left = left;
+        root->right = right;
+        return root;
+        
+    }
+};
