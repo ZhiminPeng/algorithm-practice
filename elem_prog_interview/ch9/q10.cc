8af559405e6aa47fa7153f1d5dcac0f3cc9b560d
Zhimin Peng <zhiminp@gmail.com>
2015-08-17 21:26:59 -0700

    added iterative and recursive solution for preorder traversal

---
+++ elem_prog_interview/ch9/q10.cc
@@ -0,0 +1,49 @@
+// implement preorder and post order traversal
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+// recursive solution
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if(root == NULL) return result;
+        result.push_back(root->val);
+        vector<int> left = preorderTraversal(root->left);
+        vector<int> right = preorderTraversal(root->right);
+        result.insert(result.end(), left.begin(), left.end());
+        result.insert(result.end(), right.begin(), right.end());
+        return result;
+    }
+};
+
+
+// iterative solution
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        TreeNode* itr = root;
+        stack<TreeNode*> my_stack;
+        vector<int> result;
+        if(itr == NULL) return result;
+        my_stack.push(itr);
+        while (!my_stack.empty()) {
+            itr = my_stack.top();
+            result.push_back(itr->val);
+            my_stack.pop();
+            if (itr->right) {
+                my_stack.push(itr->right);
+            }
+            if (itr->left) {
+                my_stack.push(itr->left);
+            }
+        }
+        return result;
+    }
+};
